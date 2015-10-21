76187b86eb8477df57c0f65611d2295cd481e3c8
dnowak <dawid.nowak886@gmail.com>
2015-10-21 15:23:53 +0200

    some changes in names and structure

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     @RequestMapping("/")
     public String welcome(Model model) {
 
-        model.addAttribute("greeting", "Welcome to Web Store!");
+        model.addAttribute("greeting", "Welcoooome to Web Store!");
         model.addAttribute("tagline", "The one and only amazing web store");
         return "welcome";
 
